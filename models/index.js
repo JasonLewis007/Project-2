7dcc8a6c85092cc9cd5d4ef23d122608e67a8a19
AlfredoJi <mralfredojimenez1996@gmail.com>
2023-05-02 21:40:25 -0400

    added restaurant model in models folder

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const User = require('./User');
 const Review = require('./Review');
 const Reservation = require('./Reservation');
+const Restaurant = require('./Restaurant');
 
 User.hasMany(Review, {
     foreignKey: 'user_id',
@@ -18,8 +19,17 @@ Reservation.belongsTo(User, {
     foreignKey: 'reservation_id',
 });
 
+Restaurant.hasMany(Reservation, {
+    foreignKey: 'restaurant_id',
+});
+
+Restaurant.hasMany(Review, {
+    foreignKey: 'review_id',
+});
+
 module.exports = {
     User,
     Review,
-    Reservation
+    Reservation,
+    Restaurant
 };
