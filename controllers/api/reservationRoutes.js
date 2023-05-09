1bfe8d8c89e04ee7ce4d01bced54d1ac7c19c0c7
tigergiangnguyen <tigernguyen5@yahoo.com>
2023-05-08 21:35:51 -0400

    Nodemailer creates an email with restaurant value

--- controllers/api/reservationRoutes.js
+++ controllers/api/reservationRoutes.js
@@ -2,6 +2,9 @@ const router = require('express').Router();
 const { Reservation } = require('../../models');
 const withAuth = require('./../../utils/auth');
 
+const nodeMailer = require('nodemailer');
+require('dotenv').config();
+
 router.post('/', withAuth, async (req, res) => {
     try {
         const newReservation = await Reservation.create({
@@ -10,7 +13,34 @@ router.post('/', withAuth, async (req, res) => {
             last_name: req.body.last_name,
             phone_number: req.body.phone_number,
             user_id: req.session.user_id,
+            restaurant_name: req.body.restaurant_name
         });
+        let transporter = nodeMailer.createTransport({
+            service: 'gmail',
+            auth: {
+              type: 'OAuth2',
+              user: process.env.MAIL_USERNAME,
+              pass: process.env.MAIL_PASSWORD,
+              clientId: process.env.OAUTH_CLIENTID,
+              clientSecret: process.env.OAUTH_CLIENT_SECRET,
+              refreshToken: process.env.OAUTH_REFRESH_TOKEN
+            }
+          });
+          
+          let mailOptions = {
+            from: 'tablespotter@gmail.com',
+            to: req.body.email,
+            subject: 'Table Spotter Reservation',
+            text: `You created a reservation at ${req.body.restaurant_name}`
+          };
+          
+          transporter.sendMail(mailOptions, function(err, data) {
+            if (err) {
+              console.log("Error " + err);
+            } else {
+              console.log("Email sent successfully");
+            }
+          });
 
 
         // if the user is successfully created, the new response will be returned as json
