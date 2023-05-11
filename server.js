ddb0ae988ff2f42cbd7cbb2dbaa2d0f8523f5683
Jason Lewis <pensfan5050@gmail.com>
2023-05-11 13:43:35 -0400

    push heroku

--- server.js
+++ server.js
@@ -42,10 +42,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 //sync our sequelize models with the MySQL db aand start the server
-/* sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}`));
-}); */
-
-sequelize.sync().then(() => {
+sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}`));
 });
+
